477352f3209e9a8f1a6da577b27122ff4240a7de
LennikGamez <leander@doerk.org>
2024-07-12 17:57:54 +0200

    setup router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
+import LibraryView from "./views/library-view.vue";
+
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
 
-createApp(App).mount('#app')
+
+const routes = [
+    { path: "/", component: LibraryView }
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes
+})
+
+createApp(App).use(router).mount('#app')
---
+++ src/views/library-view.vue
@@ -0,0 +1,20 @@
+
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <nav>
+        <h1>Library</h1>
+        
+    </nav>
+    <div>
+        
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
