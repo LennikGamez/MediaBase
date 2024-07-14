91e4cb5817b37641a784495dfd4c6800a05ef744
LennikGamez <leander@doerk.org>
2024-07-14 19:17:03 +0200

    created the poster request and display for each entry component

--- src/components/media-component.vue
+++ src/components/media-component.vue
@@ -1,12 +1,22 @@
 <script lang="ts" setup>
+    import { Media } from '../types';
+    import { ref } from 'vue';
 
-    
+    const props = defineProps({
+        data: {
+            type: Object as () => Media,
+            required: true
+        }
+    });
+
+    const posterLink = ref("http://localhost:8000/poster/" + props.data.entryID);
 
 </script>
 
 <template>
     <div>
-        
+        <img :src="posterLink">
+        <h2>{{ props.data.name }}</h2>
     </div>
 </template>
 
