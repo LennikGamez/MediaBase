296b26fb8757a6be068e951952a53bcdd0571717
LennikGamez <leander@doerk.org>
2024-07-15 15:27:37 +0200

    current routed path is now visible in url

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import LibraryView from "./views/library-view.vue";
+import DetailView from "./views/detail-view.vue";
 
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 
 const routes = [
-    { path: "/", component: LibraryView }
+    { path: "/", component: LibraryView },
+    { path: "/detail/:entryID", component: DetailView }
 ]
 
 const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes
 })
 
