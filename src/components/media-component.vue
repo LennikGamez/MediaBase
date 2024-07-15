5a55f50c963247fb977a49ba1c3f20445f77963e
LennikGamez <leander@doerk.org>
2024-07-15 15:07:01 +0200

    created looks for media component

--- src/components/media-component.vue
+++ src/components/media-component.vue
@@ -14,12 +14,79 @@
 </script>
 
 <template>
-    <div>
+    <div class="media">
         <img :src="posterLink">
-        <h2>{{ props.data.name }}</h2>
+        <h4 class="title">{{ props.data.name }}</h4>
     </div>
 </template>
 
 <style scoped>
+    .media{
+        --hover-shadow: 0%;
+        --default-shadow: 60%;
+        --border-radius: 8px;
+        --hover-speed: 0.5s;
+        --resize: 30px;
+        --size: 300px;
 
+        width: var(--size);
+        height: fit-content;
+        position: relative;
+        overflow-wrap: anywhere;
+
+        transition: width var(--hover-speed), margin var(--hover-speed);
+    }
+
+    img{
+        display: block;
+        width: 100%;
+        border-radius: var(--border-radius); 
+    }
+
+    .title{
+        position: absolute;
+        bottom: 0;
+        max-width: var(--size);
+        
+        color: white;
+        position: absolute;
+        bottom: 0;
+        padding: 5px;
+        margin: 10px;
+        font-size: 1.5rem;
+        pointer-events: none;
+        transition: font-size var(--hover-speed);
+    }
+
+    .media:hover{
+        width: calc(var(--size) + var(--resize));
+
+    }
+
+    .media:hover .title{
+        font-size: 1.85rem;
+    }
+
+    .media::before{
+        content: '';
+        border-radius: var(--border-radius);
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        bottom: 0;
+
+        background: -moz-linear-gradient(top, rgba(255, 255, 255, 0) var(--default-shadow),rgba(0,0,0,1) 100%);
+        background: linear-gradient(top, rgba(255, 255, 255, 0) var(--default-shadow),rgba(0,0,0,1) 10%);
+        background: -webkit-linear-gradient(top, rgba(255, 255, 255, 0) var(--default-shadow),rgba(0,0,0,1) 100%);
+        opacity: 0.8;
+
+        transition: opacity 0.5s;
+    }
+
+    .media:hover::before{
+        opacity: 1;
+        background: -moz-linear-gradient(top, rgba(255, 255, 255, 0) var(--hover-shadow),rgba(0,0,0,1) 100%);
+        background: linear-gradient(top, rgba(255, 255, 255, 0) var(--hover-shadow),rgba(0,0,0,1) 10%);
+        background: -webkit-linear-gradient(top, rgba(255, 255, 255, 0) var(--hover-shadow),rgba(0,0,0,1) 100%);
+    }
 </style>
