1ca4733017934635b246282a8d326b00756351ed
LennikGamez <leander@doerk.org>
2024-07-15 16:07:16 +0200

    created basic layout

--- src/views/detail-view.vue
+++ src/views/detail-view.vue
@@ -1,14 +1,37 @@
 <script lang="ts" setup>
-
+    import VideoPlayer from '../components/video-player.vue';
 </script>
 
 <template>
-    <div>
-        <h1>Details</h1>
-        <h4>{{ $route.params.entryID }}</h4>
+    <div id="wrapper">
+        <VideoPlayer />
+        <div id="details">
+            <h1 id="title">S.W.A.T</h1>
+            <p id="duration">1:30h</p>
+            <p id="description">Description</p>
+            <div id="buttons">
+                <button id="play-btn" class="btn focusable" tabindex="0">Play</button>
+                <select title="versions" id="version-btn" class="btn focusable" tabindex="0">
+                </select>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
+    #wrapper{
+        @media only screen and (min-width: 1100px) {
+            display: grid;
+            height: 100%;
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr;
+            place-items: center;
+            column-gap: 10px;
+        }
+    }
 
+    .player{
+        width: 100%;
+        height: 100%;
+    }
 </style>
