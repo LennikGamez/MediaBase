c072c0c40ed316fe21aab41c5f3aeedc1aa08c75
LennikGamez <leander@doerk.org>
2024-07-15 19:04:22 +0200

    detailed types of responses

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,39 @@
 
 export type Media = {
     name: string,
-    entryID: number
+    entryID: number,
+    type: number
+}
+
+export type Detail = {
+    detail: {
+        name: string,
+        entryID: number,
+        description: string,
+        posterPath: string,
+        type: number // 0 = movie, 1 = show
+    }
+}
+
+export type Episode = {
+    episodeID: number,
+    name: string
+    description: string
+    season: number
+}
+
+export type DetailShow = Detail & {
+    seasons: {
+        [key: number]: Episode[]
+    }
+}
+
+export type DetailMovie = Detail & {
+    movieData: {
+        videoID: number,
+        videoPath: string,
+        language: string,
+        movieID: number,
+        episodeID: number | null
+    }
 }
