25bb35eba75d116577e16b1dfc49f5501a8bfb9d
LennikGamez <leander@doerk.org>
2024-07-19 14:17:02 +0200

    and html track element which fetches its subtitle data on creation

---
+++ src/components/subtitle-component.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+    const props = defineProps({
+        subID: Number
+    })
+
+    const src = 'http://localhost:8000/subtitle/' + props.subID;
+
+    const blobSrc = ref('');
+    const subLang = ref('');
+
+    function fetchSubtitles(){
+        if (!props.subID) return;
+        fetch(src).then(res => res.json()).then(data => loadSubtitle(data))
+    }
+
+    function loadSubtitle(data: {[key: string]: string}) {
+        if (!props.subID) return;
+        let blob = new Blob([data["data"]], { type: 'text/vtt' });
+        const url = URL.createObjectURL(blob);
+        blobSrc.value = url;
+        subLang.value = data["language"];
+    }
+
+    fetchSubtitles();
+</script>
+
+
+<template>
+    <track default kind="subtitles" :src="blobSrc" :srclang="subLang">
+</template>
+
+
+<style scoped>
+
+</style>
