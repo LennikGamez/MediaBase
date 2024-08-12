983f227553de4aca0bb27d4521d4513823d768e4
LennikGamez <leander@doerk.org>
2024-08-12 14:12:25 +0200

    basic chapter component init

---
+++ src/components/chapter-component.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    const props = defineProps({
+        name: String,
+        num: Number,
+        id: Number
+    });
+</script>
+
+
+<template>
+    <div id="wrapper">
+        <span>{{ props.num }}</span>
+        <span>{{ props.name }}</span>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
