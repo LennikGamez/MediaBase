dbef62d93a5f3e5f7fa351838d1de9a458e04a9a
Leander <leander@doerk.org>
2024-12-26 13:09:56 +0100

    subtitles now get newly fetched as soon as a new episode starts

--- src/components/subtitle-component.vue
+++ src/components/subtitle-component.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
-    import { ref } from 'vue';
+    import { ref, watch } from 'vue';
     const props = defineProps({
         subID: Number
     })
-
-    const src = 'http://192.168.178.120:8000/subtitle/' + props.subID;
+    const subID = ref(props.subID);
+    const baseURL = "http://192.168.178.120:8000/subtitle/"
 
     const blobSrc = ref('');
     const subLang = ref('');
 
     function fetchSubtitles(){
         if (!props.subID) return;
-        fetch(src).then(res => res.json()).then(data => loadSubtitle(data))
+        fetch(baseURL + subID.value).then(res => res.json()).then(data => loadSubtitle(data))
     }
 
     function loadSubtitle(data: {[key: string]: string}) {
+        console.log("load");
         if (!props.subID) return;
         let blob = new Blob([data["data"]], { type: 'text/vtt' });
         const url = URL.createObjectURL(blob);
@@ -22,7 +23,15 @@
         subLang.value = data["language"];
     }
 
-    fetchSubtitles();
+    // Watch for changes to the subID prop and refetch subtitles if it changes
+    watch(() => props.subID, (newSubID, oldSubID) => {
+        if (newSubID !== oldSubID) {
+            subID.value = newSubID;
+            fetchSubtitles();
+        }
+    },
+    { immediate: true } // Immediately run fetchSubtitles on mount
+    );
 </script>
 
 
