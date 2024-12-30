4dec4fdf4206df7fa35473f445a01f2a46197063
Leander <leander@doerk.org>
2024-12-30 16:13:02 +0100

    added underscores to unused parameters

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior(_to, _from, savedPosition) {
     // If saved position exists, restore it
     if (savedPosition) {
       return new Promise((resolve) => {
