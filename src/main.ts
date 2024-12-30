9519746b2b32f05d88891d8d606f2a1caa4d5963
Leander <leander@doerk.org>
2024-12-30 15:56:42 +0100

    scroll position is remembered

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,32 @@ import LibraryView from "./views/library-view.vue";
 import DetailView from "./views/detail-view.vue";
 import audioView from "./views/audio-view.vue";
 
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-    { path: "/", component: LibraryView },
-    { path: "/audio/:entryID", component: audioView },
-    { path: "/detail/:entryID/:type", component: DetailView }
-]
+  { path: "/", component: LibraryView },
+  { path: "/audio/:entryID", component: audioView },
+  { path: "/detail/:entryID/:type", component: DetailView },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    console.log("scrolling to", savedPosition);
+    // If saved position exists, restore it
+    if (savedPosition) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({top: savedPosition.top , behavior: 'smooth'});
+        }, 500);
+      })
+    }
 
-createApp(App).use(router).mount('#app')
+    return { top: 0 }; // Scroll to top by default
+  }
+});
+createApp(App).use(router).mount("#app");
