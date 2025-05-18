8a17866d09863c1bd857466671af3120e5dcfc62
Leander <leander@doerk.org>
2025-05-18 22:50:30 +0200

    each poster now has an aspect ratio of 2/3 without using the css property for browser support. A nice shadow and a new scaling effect on hover. New layout in the libarary view

--- src/components/media-component.vue
+++ src/components/media-component.vue
@@ -1,110 +1,151 @@
 <script lang="ts" setup>
-    import { useRouter } from 'vue-router';
-    import { Media } from '../types';
-    import { ref } from 'vue';
+import { useRouter } from "vue-router";
+import { Media } from "../types";
+import { ref } from "vue";
 
-    const router = useRouter();
-    const props = defineProps({
-        data: {
-            type: Object as () => Media,
-            required: true
-        }
-    });
+const router = useRouter();
+const props = defineProps({
+  data: {
+    type: Object as () => Media,
+    required: true,
+  },
+});
 
-    const posterLink = ref("http://192.168.178.120:8000/poster/" + props.data.entryID);
+const posterLink = ref(
+  "http://192.168.178.120:8000/poster/" + props.data.entryID,
+);
 
-    const wrapper = ref(HTMLDivElement)
+const wrapper = ref(HTMLDivElement);
 
-    function rerouteToDetails(){
-        console.log("reroute to details of " + props.data.name);
-        switch (props.data.type) {
-            case 0:
-            case 1:
-                router.push({path: "/detail/" + props.data.entryID + "/" + props.data.type});
-                break;
-            case 2:
-                router.push({path: "/audio/" + props.data.entryID});
-                break;
-
-        }
-    }
-
+function rerouteToDetails() {
+  console.log("reroute to details of " + props.data.name);
+  switch (props.data.type) {
+    case 0:
+    case 1:
+      router.push({
+        path: "/detail/" + props.data.entryID + "/" + props.data.type,
+      });
+      break;
+    case 2:
+      router.push({ path: "/audio/" + props.data.entryID });
+      break;
+  }
+}
 </script>
 
 <template>
-    <div class="media" ref="wrapper" @click="rerouteToDetails">
-        <img :src="posterLink" loading="lazy">
-        <h4 class="title">{{ props.data.name }}</h4>
-    </div>
+  <div class="media" ref="wrapper" @click="rerouteToDetails">
+    <img :src="posterLink" loading="lazy" />
+    <h4 class="title">{{ props.data.name }}</h4>
+  </div>
 </template>
 
 <style scoped>
-    .media{
-        --hover-shadow: 0%;
-        --default-shadow: 60%;
-        --border-radius: 8px;
-        --hover-speed: 0.5s;
-        --resize: 30px;
-        --size: 300px;
+.media {
+  --hover-shadow: 0%;
+  --default-shadow: 60%;
+  --border-radius: 8px;
+  --hover-speed: 0.7s;
 
-        width: var(--size);
-        height: fit-content;
-        position: relative;
-        overflow-wrap: anywhere;
+  width: 100%;
+  padding-bottom: 150%; /* aspect-ratio is not supported on webOS TV :C */
+  position: relative;
+  overflow-wrap: word;
+  z-index: 100;
 
-        transition: width var(--hover-speed), margin var(--hover-speed);
-    }
+  transition:
+    transform var(--hover-speed),
+    -webkit-box-shadow var(--hover-speed) ease-in-out;
+}
 
-    img{
-        display: block;
-        width: 100%;
-        border-radius: var(--border-radius); 
-    }
+img {
+  display: block;
+  border-radius: var(--border-radius);
+  object-fit: cover;
 
-    .title{
-        position: absolute;
-        bottom: 0;
-        max-width: var(--size);
-        
-        color: white;
-        position: absolute;
-        bottom: 0;
-        padding: 5px;
-        margin: 10px;
-        font-size: 1.5rem;
-        pointer-events: none;
-        transition: font-size var(--hover-speed);
-    }
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  /* also aspect-ratio replacement */
+}
 
-    .media:hover{
-        width: calc(var(--size) + var(--resize));
+.title {
+  position: absolute;
+  bottom: 0;
+  max-width: 100%;
 
-    }
+  color: white;
+  position: absolute;
+  bottom: 0;
+  padding: 5px;
+  margin: 10px;
+  font-size: 1.5rem;
+  pointer-events: none;
 
-    .media:hover .title{
-        font-size: 1.85rem;
-    }
+  z-index: 501;
+}
 
-    .media::before{
-        content: '';
-        border-radius: var(--border-radius);
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        bottom: 0;
+@media (hover: hover) {
+  /* only on devices without touchscreen */
+  .media:hover {
+    --shadow-color: rgba(121, 9, 74, 1);
+    transform: scale(1.3);
+    z-index: 101;
 
-        background: -moz-linear-gradient(top, rgba(255, 255, 255, 0) var(--default-shadow),rgba(0,0,0,1) 100%);
-        background: linear-gradient(top, rgba(255, 255, 255, 0) var(--default-shadow),rgba(0,0,0,1) 10%);
-        background: -webkit-linear-gradient(top, rgba(255, 255, 255, 0) var(--default-shadow),rgba(0,0,0,1) 100%);
-        opacity: 0.8;
+    box-shadow: 0px 10px 44px 16px var(--shadow-color);
+    -moz-box-shadow: 0px 10px 44px 16px var(--shadow-color);
+    -webkit-box-shadow: 0px 10px 44px 16px var(--shadow-color);
+    border-radius: var(--border-radius);
+  }
+}
 
-        transition: opacity 0.5s;
-    }
+.media::before {
+  content: "";
+  border-radius: var(--border-radius);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  bottom: 0;
+  z-index: 500;
 
-    .media:hover::before{
-        opacity: 1;
-        background: -moz-linear-gradient(top, rgba(255, 255, 255, 0) var(--hover-shadow),rgba(0,0,0,1) 100%);
-        background: linear-gradient(top, rgba(255, 255, 255, 0) var(--hover-shadow),rgba(0,0,0,1) 10%);
-        background: -webkit-linear-gradient(top, rgba(255, 255, 255, 0) var(--hover-shadow),rgba(0,0,0,1) 100%);
-    }
+  background: -moz-linear-gradient(
+    top,
+    rgba(255, 255, 255, 0) var(--default-shadow),
+    rgba(0, 0, 0, 1) 100%
+  );
+  background: linear-gradient(
+    top,
+    rgba(255, 255, 255, 0) var(--default-shadow),
+    rgba(0, 0, 0, 1) 10%
+  );
+  background: -webkit-linear-gradient(
+    top,
+    rgba(255, 255, 255, 0) var(--default-shadow),
+    rgba(0, 0, 0, 1) 100%
+  );
+  opacity: 0.8;
+
+  transition: opacity 0.5s;
+}
+
+.media:hover::before {
+  opacity: 1;
+  background: -moz-linear-gradient(
+    top,
+    rgba(255, 255, 255, 0) var(--hover-shadow),
+    rgba(0, 0, 0, 1) 100%
+  );
+  background: linear-gradient(
+    top,
+    rgba(255, 255, 255, 0) var(--hover-shadow),
+    rgba(0, 0, 0, 1) 10%
+  );
+  background: -webkit-linear-gradient(
+    top,
+    rgba(255, 255, 255, 0) var(--hover-shadow),
+    rgba(0, 0, 0, 1) 100%
+  );
+}
 </style>
