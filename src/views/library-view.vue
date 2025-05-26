5194e6dc5b7a469d25e7a5eef981b2df68543f99
Leander <104307958+LennikGamez@users.noreply.github.com>
2025-05-26 20:18:42 +0200

    Library poster aspect ratio fix (#4)
    * each poster now has an aspect ratio of 2/3 without using the css property for browser support. A nice shadow and a new scaling effect on hover. New layout in the libarary view
    
    * fix: library view layout
    
    * set the maximum width of a media entry to 400px and the maximum height to 600px to stop a single media component to take up all the available space if it is the only one in the list
    
    * changed the layout and the style of the navbar
    
    * added text feedback to let user know nothing was found and restructured the filter functionality
    
    * skeleton loading effect on the library images

--- src/views/library-view.vue
+++ src/views/library-view.vue
@@ -1,69 +1,90 @@
-
 <script setup lang="ts">
-    import MediaComponent from "../components/media-component.vue";
-    import { Media } from "../types";
-    import { ref } from "vue";
-    
-    const media = ref(Array<Media>());
-    const searchInput = ref(""); 
-    function fetchMedia(){
-        fetch('http://192.168.178.120:8000/media').then(res => res.json()).then(data => media.value = data)
-    }
+import MediaComponent from "../components/media-component.vue";
+import { Media } from "../types";
+import { computed, ref } from "vue";
 
-    fetchMedia();
+const media = ref(Array<Media>());
+const searchInput = ref("");
+function fetchMedia() {
+  fetch("http://192.168.178.120:8000/media")
+    .then((res) => res.json())
+    .then((data) => (media.value = data));
+}
 
+fetchMedia();
 
-    function stringIncludes(a: string, b: string){
-        // splits  string into array and checks if each word is included in the other string
-        return a.split(" ").every(word => b.includes(word));
-    }
+function stringIncludes(a: string, b: string) {
+  // splits  string into array and checks if each word is included in the other string
+  return a.split(" ").every((word) => b.includes(word));
+}
 
+const filteredMediaItems = computed(() => {
+  return media.value.filter((m) =>
+    stringIncludes(searchInput.value.toLowerCase(), m.name.toLowerCase()),
+  );
+});
 </script>
 
-
 <template>
-    <div class="grid">
-        <nav>
-            <h1>Library</h1>
-            <input id="searchbar" type="etxt" placeholder="Search.." v-model="searchInput"/>
-        </nav>
-        <div id="library-container">
-            <MediaComponent v-for="m in media.filter(m => stringIncludes(searchInput.toLowerCase(), m.name.toLowerCase()))" :key="m.name" :data="m"/>
-        </div>
+  <div class="grid">
+    <nav>
+      <h1>MediaBase</h1>
+      <input
+        id="searchbar"
+        type="etxt"
+        placeholder="Search.."
+        v-model="searchInput"
+      />
+    </nav>
+    <div id="library-container">
+      <h4 v-if="filteredMediaItems.length == 0">Nothing here...</h4>
+      <MediaComponent v-for="m in filteredMediaItems" :key="m.name" :data="m" />
     </div>
+  </div>
 </template>
 
-
 <style scoped>
+nav {
+  z-index: 1000;
+  position: fixed;
+  top: 0px;
+  box-shadow: 0px 12px 12px 0px rgba(0, 0, 0, 0.943);
+  width: 100%;
+  height: 7rem;
+  background-color: var(--bg-color);
 
-    nav{
-        z-index: 2;
-        position: fixed;
-        top: 0px;
-        box-shadow: 0px 12px 12px 0px rgba(0, 0, 0, 0.943);
-        width: 100%;
-        height: 7rem;
-        background-color: var(--bg-color);
-    }
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  place-items: center;
+}
 
-    #library-container{
-        margin: 16px;
-        margin-top: 10rem;
+#searchbar {
+  outline: none;
+  border: none;
 
-        display: grid;
-        gap: 16px;
-        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-        place-items: center;
-        height: auto;
-        /* display: flex;
-        gap: 16px;
-        flex-wrap: wrap;
-        justify-content: start; */
-        scrollbar-width: none;
-        -ms-overflow-style: none;
-    }
+  padding: 8px 16px;
+  border-radius: 8px;
+  width: 200px;
 
-    #library-container::-webkit-scrollbar{
-        display: none;
-    }
+  transition: width 1s;
+}
+
+#searchbar:focus {
+  width: 500px;
+}
+
+#library-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 1.7rem;
+  place-items: center;
+  margin: calc(7rem + 16px) 10vw;
+
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+
+#library-container::-webkit-scrollbar {
+  display: none;
+}
 </style>
