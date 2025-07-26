9e3366dc612bcca687c2694fa4fb58fe1031aa4c
Leander <leander@doerk.org>
2025-07-26 12:20:41 +0200

    when the media elemetn is pressed, if it is a group it reroutes to the library view applying the new group to the urlquery, if it is not a group it navigates to the detail view and passes the group in the query

--- src/components/media-component.vue
+++ src/components/media-component.vue
@@ -1,39 +1,64 @@
-, setPosterPath<script lang="ts" setup>
-    import { useRouter } from 'vue-router';
-    import { Media } from '../types';
-    import { ref } from 'vue';
-    import APIConnector from '../helper/APIConnector'; 
+, setPosterPath
+<script lang="ts" setup>
+import { useRoute, useRouter } from "vue-router";
+import { Media } from "../types";
+import { ref } from "vue";
+import APIConnector from "../helper/APIConnector";
 
-    const router = useRouter();
-    const props = defineProps({
-        data: {
-            type: Object as () => Media,
-            required: true
-        }
+const router = useRouter();
+const props = defineProps({
+  data: {
+    type: Object as () => Media,
+    required: true,
+  },
+});
+
+const route = useRoute();
+const posterLink = ref(
+  APIConnector.getPosterURL(
+    props.data.group || props.data.name, // either get the poster of the group or the poster of the entry name
+    props.data.type,
+    route.query.group as string,
+  ),
+);
+
+const wrapper = ref(HTMLDivElement);
+
+function onClick() {
+  if (props.data.group) {
+    // if the media component is representing a group the router navigates to library view with the new group as query
+    router.push({
+      path: "/",
+      query: {
+        group: props.data.group,
+        grouptype: props.data.type,
+      },
     });
+    return;
+  }
+  rerouteToDetails();
+}
 
-    const posterLink = ref(APIConnector.getPosterURL(props.data.name, props.data.type));
-
-    const wrapper = ref(HTMLDivElement)
-
-    function rerouteToDetails(){
-        console.log("reroute to details of " + props.data.name);
-        switch (props.data.type) {
-            case 0:
-            case 1:
-                router.push({path: "/detail/" + props.data.name+ "/" + props.data.type});
-                break;
-            case 2:
-                router.push({path: "/audio/" + props.data.name});
-                break;
-
-        }
-    }
-
+function rerouteToDetails() {
+  console.log("reroute to details of " + props.data.name);
+  let group = (route.query.group as string) || "";
+  switch (props.data.type) {
+    case 0:
+    case 1:
+      router.push({
+        path: `/detail/${props.data.name}/${props.data.type}`,
+        query: { group },
+      });
+      break;
+    case 2:
+      router.push({ path: "/audio/" + props.data.name, query: { group } });
+      break;
+  }
+}
 </script>
 
 <template>
-  <div class="media" ref="wrapper" @click="rerouteToDetails">
+  <div class="media" ref="wrapper" @click="onClick">
     <img :src="posterLink" loading="lazy" class="skeleton" />
     <h4 class="title">{{ props.data.name }}</h4>
   </div>
